0e8f7132152884f0a0ae919ae574d26a1f566a55
TonicBox <gsec.tonicbox@protonmail.com>
2025-09-15 18:09:35 -0500

    init

---
+++ src/main/java/com/tonic/woodcutter/ExamplePlugin.java
@@ -0,0 +1,108 @@
+package com.tonic.woodcutter;
+
+import com.tonic.Logger;
+import com.tonic.Static;
+import com.tonic.api.entities.PlayerAPI;
+import com.tonic.api.entities.TileObjectAPI;
+import com.tonic.api.widgets.InventoryAPI;
+import com.tonic.data.ItemContainerEx;
+import com.tonic.data.ItemEx;
+import com.tonic.data.TileObjectEx;
+import com.tonic.services.ClickManager;
+import com.tonic.util.VitaPlugin;
+import net.runelite.api.Client;
+import net.runelite.api.Player;
+import net.runelite.api.gameval.InventoryID;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
+import org.w3c.dom.css.Rect;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+@PluginDescriptor(
+        name = "Vita Wood Chopper Pro",
+        description = "A sample VitaLite Plugin",
+        tags = {"vita", "sample", "woodcutter", "wood", "chopper", "pro"}
+)
+public class ExamplePlugin extends VitaPlugin
+{
+    @Inject
+    private ClientToolbar clientToolbar;
+    @Inject
+    private Client client;
+
+    private SidePanel panel;
+    private NavigationButton navButton;
+
+    @Override
+    protected void startUp()
+    {
+        panel = injector.getInstance(SidePanel.class);
+
+        final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "icon.png");
+
+        navButton = NavigationButton.builder()
+                .tooltip("Example Vita Woodcutting Plugin")
+                .icon(icon)
+                .priority(5)
+                .panel(panel)
+                .build();
+
+        clientToolbar.addNavigation(navButton);
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        clientToolbar.removeNavigation(navButton);
+        panel.shutdown();
+    }
+
+    @Override
+    public void loop()
+    {
+        if(panel == null || !panel.isRunning())
+            return;
+
+        Player local = client.getLocalPlayer();
+        if(!PlayerAPI.isIdle(local))
+            return;
+
+        if(InventoryAPI.isFull())
+        {
+            ItemContainerEx container = new ItemContainerEx(InventoryID.INV);
+            List<ItemEx> items = container.getAll("Logs");
+            for(ItemEx item : items)
+            {
+                ClickManager.queueClickBox(Static.getRuneLite().getGameApplet().getSideMenuArea());
+                InventoryAPI.interact(item, "Drop");
+            }
+            return;
+        }
+
+        TileObjectEx tree = TileObjectAPI.get("Tree");
+        if(tree == null)
+        {
+            Logger.warn("Could not find a tree!");
+            return;
+        }
+
+        ClickManager.queueClickBox(getClickBox(tree));
+        TileObjectAPI.interact(tree, "Chop down");
+    }
+
+    private Rectangle getClickBox(TileObjectEx object)
+    {
+        Shape shape = object.getTileObject().getClickbox();
+        if(shape != null)
+        {
+            return shape.getBounds();
+        }
+        return Static.getRuneLite().getGameApplet().getWorldViewportArea();
+    }
+}
---
+++ src/main/java/com/tonic/woodcutter/ExamplePluginConfig.java
@@ -0,0 +1,7 @@
+package com.tonic.woodcutter;
+
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("exampleplugin")
+public interface ExamplePluginConfig {
+}
